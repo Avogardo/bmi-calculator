e874ee8efb4379eefe49e1b6543dab99f936c883
Avogardo <jakub.i.wolny@gmail.com>
2019-01-19 14:46:31 +0100

    add simple caloric form

--- client/src/components/Training.vue
+++ client/src/components/Training.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <md-dialog-title>Trainings</md-dialog-title>
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Trainings</div>
+      </md-card-header>
 
+      <md-card-content>
+        Here you can add your today burned calories from training
+      </md-card-content>
+
+      <md-card-actions>
+        <md-field>
+          <label>Calories</label>
+          <md-input v-model="trainingsCalories" type="number"></md-input>
+        </md-field>
+        <md-button>Save</md-button>
+      </md-card-actions>
+    </md-card>
   </div>
 </template>
 
 <script>
   export default {
     name: "Training",
+    data() {
+      return {
+        trainingsCalories: 0,
+      };
+    },
   }
 </script>
